30c86db76ce73e09f1472fcf5554e7d4ddea4de7
LVNAnh <levunhatanh1997@gmail.com>
2025-06-12 12:12:23 +0700

    cap nhat validate cho register va login

--- src/validators/auth.validator.js
+++ src/validators/auth.validator.js
@@ -1,7 +1,16 @@
 const Joi = require("joi");
 
 exports.registerSchema = Joi.object({
-  username: Joi.string().min(3).required(),
-  email: Joi.string().email().required(),
+  username: Joi.string().min(3).max(30).required(),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
   password: Joi.string().min(6).required(),
+});
+
+exports.loginSchema = Joi.object({
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
+  password: Joi.string().required(),
 });
